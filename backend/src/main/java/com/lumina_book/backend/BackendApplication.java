42d5ebd5e21b0167e7debf6a5aa2ebbcd95977a5
LinhDev610 <binbicute1997@gmail.com>
2025-11-20 15:49:49 +0700

    fix momoApi on MomoService.java

--- backend/src/main/java/com/lumina_book/backend/BackendApplication.java
+++ backend/src/main/java/com/lumina_book/backend/BackendApplication.java
@@ -2,10 +2,12 @@ package com.lumina_book.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableFeignClients(basePackages = "com.lumina_book.backend.client")
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);

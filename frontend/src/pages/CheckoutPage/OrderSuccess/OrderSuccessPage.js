b4f88c9b326100fc572395c2c1e4c26da7d8d6ad
leanhvu17023010 <leanhvu4002@gmail.com>
2025-11-21 13:42:00 +0700

    test

--- frontend/src/pages/CheckoutPage/OrderSuccess/OrderSuccessPage.js
+++ frontend/src/pages/CheckoutPage/OrderSuccess/OrderSuccessPage.js
@@ -4,6 +4,7 @@ import classNames from 'classnames/bind';
 import styles from './OrderSuccessPage.module.scss';
 
 const cx = classNames.bind(styles);
+ 
 
 const formatPrice = (value) =>
     new Intl.NumberFormat('vi-VN', {
